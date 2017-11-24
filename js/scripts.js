a1e714997eb6afb55f64cb9da85658fcbb84e9b5
Matthew Miller <mrmiller912@gmail.com>
2017-11-23 19:13:50 -0500

    now adding responsive cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,56 @@
+
+
+
+/*paths for replacing status bar images*/
+var sb1 = "images/sb/sb1.png";
+var sb2 = "images/sb/sb2.png";
+var sb3 = "images/sb/sb3.png";
+var sb4 = "images/sb/sb4.png";
+
+sbArray =[sb1,sb2,sb3,sb4,]; /* arrawy of status bar images*/
+var sbIndex = 0;/* inital position for cycling through status bar array*/
+
+
 $(document).ready(function() {
 
-    var $item = $('.item'), //Cache your DOM selector
-        visible = 1, //Set the number of items that will be visible
-        index = 0, //Starting index
-        endIndex = ( $item.length / visible ) - 1; //End index
-        multi =.86
-        
-    $('#arrowR').click(function(){
+    console.log(sbIndex);
 
-    var width = jQuery(window).width();
-        
-    $item.animate({left:'-='+(width*multi)});
-    multi = multi * 1.08
-    
-    });
 
-    $('#arrowL').click(function(){
-    var width = jQuery(window).width();
+    $(".next-card").click(nextCard);
+    $(".next-card").click(changeProgress);
 
-        $item.animate({left:'+='+(width*multi)});
-    multi = multi * 1.08
-
-    });
 
 });
+
+
+function nextCard() {
+
+    var $item = $('.item');
+    var buttonId = $(this).attr("id");
+    
+
+    multi =1;
+
+    var c1 = $("#c1");
+         
+    var width = c1.outerWidth(true);
+
+    console.log(width);
+        
+    $item.animate({left:'+='+(width*0.07)},"fast"); 
+    $item.animate({left:'-='+(width*1.07)},"slow");
+    /*$item.animate({left:'-='+(width)},"slow");*/
+
+    
+    
+    }
+
+    function changeProgress() {
+
+            sbIndex++;
+         if (sbIndex > 0) {
+        $("#status-bar-img").attr("src", sbArray[(sbIndex-1)]);
+
+    }
+
+}
